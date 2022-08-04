ec62bf8474d2193c9c4881abcd032099e64c2413
koslinj <koslinj@gmail.com>
2022-08-04 18:45:22 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: [],
+      active: false,
+      deleting: false
+    }
+  }
+  changeState(text) {
+    this.setState({
+      newItem: text
+    })
+  }
+  addItem() {
+    const list = [...this.state.list];
+    let actual = new Date();
+    let hours = actual.getHours();
+    let minutes = actual.getMinutes();
+    if(minutes<10){
+      minutes = '0'+minutes;
+    }
+    let item = {
+      id: 1 + Math.random(),
+      name: this.state.newItem.slice(),
+      date: `${hours}:${minutes}`
+    };
+    list.push(item);
+    this.setState({
+      list,
+      newItem: ""
+    })
+  }
+  deleteItem(id) {
+    const newList = [...this.state.list].filter(item => item.id !== id);
+    this.setState({
+      list: newList,
+      newItem: ""
+    })
+  }
+  clicked(elem) {
+
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="top">
+          <input value={this.state.newItem} onChange={e => this.changeState(e.target.value)} onKeyDown={e => {
+            if (e.key === 'Enter') {
+              this.addItem();
+              this.setState({
+                active: true
+              })
+              setTimeout(() => {
+                this.setState({
+                  active: false
+                })
+              }, 300);
+            }
+          }} placeholder="Wpisz zadanie!"></input>
+          <button style={this.state.active === false ? { opacity: '1' } : { opacity: '0.5' }} onClick={() => {
+            this.addItem()
+            this.setState({
+              active: true
+            })
+            setTimeout(() => {
+              this.setState({
+                active: false
+              })
+            }, 300);
+          }}>Dodaj</button>
+        </div>
+        <div className="todos">
+          <ul>
+            {this.state.list.map(item => {
+              return (
+                <li style={this.state.deleting === false ? { opacity: '1' } : { opacity: '0' }} key={item.id}>
+                  <div className="timeInfo">
+                    {item.date}
+                  </div>
+                  <div className="text">{item.name}</div>
+                  <button id="lol" onClick={(e) => {
+                    e.currentTarget.parentElement.style = "opacity: 0;"
+                    setTimeout(()=> this.deleteItem(item.id) ,600)
+                  }}>Usuń!</button>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App;
